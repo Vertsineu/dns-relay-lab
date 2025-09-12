2c00e3bb5e355db4454b256849754bd198078dba
Vertsineu <vertsineu@qq.com>
2025-09-12 19:56:37 +0800

    docs: update comments

--- dns_dgram_utils.c
+++ dns_dgram_utils.c
@@ -8,10 +8,12 @@
 
 /*
     parse the domain name, type and class from question section of a dns datagram
-    parameters:
+    input:
+        buf: the pointer point to the begin of the dns datagram
+    output:
         name: the resolved domain name
         question: other fields except domain name in question section
-        buf: the pointer point to the begin of the dns datagram
+    note: support both sequences of labels and pointer
 */
 void parse_question_section(char *name, dns_question_t *question, const unsigned char *buf) {
     /* 
@@ -22,11 +24,12 @@ void parse_question_section(char *name, dns_question_t *question, const unsigned
 
 /**
     try to find answer to the domain name by reading the local host file
-    parameters:
-        ip: the ip in string format (eg. "192.168.1.1")
+    input:
         name: the domain name try to answer
         question: other fields except domain name in question section
         file_path: the path to the local host file
+    output:
+        ip: the IP of multiple resource records in string format (eg. "192.168.1.1")
     return:
         0 if no record, positive if any record
     note: supports one IP mapping to multiple domain names per line
@@ -39,16 +42,20 @@ int try_answer_local(char ip[MAX_ANSWER_COUNT][MAX_IP_BUFFER_SIZE], const char *
 }
 
 /**
-    transform a dns request datagram to a dns response datagram
-    parameters:
+    it's more convenient to transform a dns request datagram to a dns response datagram than to construct a new dns response datagram
+    input:
         buf: original dns request datagram
         len: original dns request datagram length
-        ip: the the ip in string format (eg. "192.168.1.1")
-        count: how many ip bind to this domain name
+        ip: the IP of multiple resource records in string format (eg. "192.168.1.1")
+        count: how many IP bind to this domain name
         question: other fields except domain name in question section
+    output:
+        buf: new dns response datagram
     return:
         length of the new dns response datagram
-    note: do not need domain name, use domain name compression instead
+    note: 
+        - do not need domain name, use pointer instead
+        - need to support both IPv4 and IPv6
  */
 int transform_to_response(unsigned char *buf, int len, const char ip[MAX_ANSWER_COUNT][MAX_IP_BUFFER_SIZE], int count, const dns_question_t *question) {
     /* 
